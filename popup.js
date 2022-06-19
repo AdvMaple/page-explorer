dc00666606a9d398a50c8969611e2d0d33a763e0
o317p18 <o317p18@gmail.com>
2022-06-19 14:17:12 +0200

    format

--- popup.js
+++ popup.js
@@ -20,47 +20,47 @@ function run() {
     // explorer class
     class Explorer {
         data = {}
-        
+
         analyze() {
             this._resetData()
-    
+
             const elements = document.getElementsByTagName('*')
             for (let i = 0; i < elements.length; i++) {
                 // get class specific styles
                 const element = elements[i]
                 const style = getComputedStyle(element)
-    
+
                 const attributesLinkGraphic = ['href', 'src', 'backgroundImage', 'borderImage']
                 // links and graphics
-                for(let j = 0; j < attributesLinkGraphic.length; j++) {
+                for (let j = 0; j < attributesLinkGraphic.length; j++) {
                     const attr = attributesLinkGraphic[j]
-                    const elementValue  = element[attr]
+                    const elementValue = element[attr]
                     const styleValue = style[attr]
                     this._checkAndAddLink(styleValue)
                     this._checkAndAddLink(elementValue)
                 }
-    
+
                 // colors
                 const attributesColor = ['color', 'backgroundColor', 'accentColor', 'borderColor']
-                for(let j = 0; j < attributesColor.length; j++) {
+                for (let j = 0; j < attributesColor.length; j++) {
                     const color = style[attributesColor[j]]
                     this._checkAndAddColor(color)
                 }
 
                 // svg
-                if(element.tagName === 'svg') {
+                if (element.tagName === 'svg') {
                     // ignore defs
-                    if(!element.outerHTML.includes('<defs>')) {
+                    if (!element.outerHTML.includes('<defs>')) {
                         // ignore svg elements with 0 viewBox and duplicates
-                        if(!element.getAttribute('viewBox')) element.setAttribute('viewBox', '0 0 24 24')
-                        if(!this.data.svgs.includes(element.outerHTML) && element.getAttribute('viewBox') !== '0 0 0 0') this.data.svgs.push(element.outerHTML)
+                        if (!element.getAttribute('viewBox')) element.setAttribute('viewBox', '0 0 24 24')
+                        if (!this.data.svgs.includes(element.outerHTML) && element.getAttribute('viewBox') !== '0 0 0 0') this.data.svgs.push(element.outerHTML)
                     }
                 }
 
                 //classes
                 element.classList.forEach(cls => {
-                    if(cls.includes('=') || cls.includes(':')) return
-                    if(this.data.classes[cls]) {
+                    if (cls.includes('=') || cls.includes(':')) return
+                    if (this.data.classes[cls]) {
                         this.data.classes[cls].counter += 1
                     } else {
                         this.data.classes[cls] = {
@@ -75,27 +75,27 @@ function run() {
 
                 // box shadow
                 this._checkAndAddBoxShadow(style.boxShadow)
-    
+
             }
-    
+
             // sort colors by counter -> hightest to lowest
             const colorsSortable = []
-            for(const color in this.data.colors) {
-                colorsSortable.push({color: color, counter: this.data.colors[color]})
+            for (const color in this.data.colors) {
+                colorsSortable.push({ color: color, counter: this.data.colors[color] })
             }
             this.data.colors = this._sortArrayByCounter(colorsSortable)
 
             // sort colors by counter -> hightest to lowest
             const tagsSortable = []
-            for(const tagName in this.data.htmlTags) {
-                tagsSortable.push({tagName: tagName, counter: this.data.htmlTags[tagName]})
+            for (const tagName in this.data.htmlTags) {
+                tagsSortable.push({ tagName: tagName, counter: this.data.htmlTags[tagName] })
             }
             this.data.htmlTags = this._sortArrayByCounter(tagsSortable)
 
             // sort classes by counter
             const classesSortable = []
-            for(const cls in this.data.classes) {
-                classesSortable.push({className: cls, counter: this.data.classes[cls].counter, attributes: this.data.classes[cls].attributes})
+            for (const cls in this.data.classes) {
+                classesSortable.push({ className: cls, counter: this.data.classes[cls].counter, attributes: this.data.classes[cls].attributes })
             }
             this.data.classes = this._sortArrayByCounter(classesSortable)
 
@@ -106,12 +106,12 @@ function run() {
             return this.data
         }
 
-         _getClassAttributes(cls) {
+        _getClassAttributes(cls) {
             const elementTag = `element-${new Date().getTime()}`
             const defaultElement = document.createElement(elementTag)
             const classElement = document.createElement(elementTag)
             classElement.classList.add(cls)
-            
+
             document.body.appendChild(defaultElement)
             document.body.appendChild(classElement)
 
@@ -120,9 +120,9 @@ function run() {
 
             let attributes = []
 
-            for(let i = 0; i < classStyle.length; i++) {
+            for (let i = 0; i < classStyle.length; i++) {
                 const attr = classStyle[i]
-                if(classStyle[attr] !== defaultStyle[attr]) {
+                if (classStyle[attr] !== defaultStyle[attr]) {
                     attributes.push({
                         attr,
                         value: classStyle[attr]
@@ -135,36 +135,36 @@ function run() {
 
             return attributes
         }
-    
+
         _checkAndAddLink(val) {
-            if(!this._existsValue(val) || typeof(val) !== 'string') return
-            if(this._isGraphic(val)) return this.data.imageUrls.push(val)
+            if (!this._existsValue(val) || typeof (val) !== 'string') return
+            if (this._isGraphic(val)) return this.data.imageUrls.push(val)
         }
 
         _sortArrayByCounter(arr) {
             return arr.sort((a, b) => {
-                if(b.counter > a.counter) return 1
-                if(a.counter > b.counter) return -1
+                if (b.counter > a.counter) return 1
+                if (a.counter > b.counter) return -1
                 return 0
             })
         }
 
         _checkAndAddHtmlTagName(tagName) {
-            if(this.data.htmlTags[tagName]) return this.data.htmlTags[tagName] +=1
+            if (this.data.htmlTags[tagName]) return this.data.htmlTags[tagName] += 1
             this.data.htmlTags[tagName] = 1
         }
-    
+
         _checkAndAddColor(val) {
-            if(!this._existsValue(val)) return
-            if(this.data.colors[val]) return this.data.colors[val] += 1
+            if (!this._existsValue(val)) return
+            if (this.data.colors[val]) return this.data.colors[val] += 1
             this.data.colors[val] = 1
         }
 
         _checkAndAddBoxShadow(val) {
-            if(!this._existsValue(val)) return
+            if (!this._existsValue(val)) return
             this.data.boxShadows.push(val)
         }
-    
+
         _resetData() {
             this.data = {
                 imageUrls: [],
@@ -175,19 +175,19 @@ function run() {
                 classes: {}
             }
         }
-    
+
         _existsValue(val) {
             return val && val !== 'none'
         }
-    
+
         _isGraphic(url) {
-            if(typeof url !== 'string') return false
+            if (typeof url !== 'string') return false
             const IMAGE_EXTENSIONS = ['.jpg', '.png', '.svg', '.jpeg', '.apng', '.avif', '.gif', '.jfif', '.pjpeg', '.pjp', '.webp']
             return IMAGE_EXTENSIONS.filter(ext => url.includes(ext)).length > 0 || url.includes('data:image')
         }
-    
+
         banner() {
-           console.log(`%cPage Explorer ${manifestData.version}`, 'font-weight: bold; font-family: "Roboto", sans-serif;')
+            console.log(`%cPage Explorer ${manifestData.version}`, 'font-weight: bold; font-family: "Roboto", sans-serif;')
         }
     }
 
@@ -196,20 +196,20 @@ function run() {
 }
 
 function result(resultTab) {
-    if(!resultTab) return
+    if (!resultTab) return
     const result = resultTab[0].result
-    if(!result) return
+    if (!result) return
 
     generateSidebar(result)
     generateSections(result)
 
     // insert elements
     const dataKeys = Object.keys(result)
-    for(let i = 0; i < dataKeys.length; i++) {
+    for (let i = 0; i < dataKeys.length; i++) {
         const identifier = dataKeys[i]
         const section = document.getElementById(`section-${identifier}`)
         section.innerHTML = ''
-        for(let j = 0; j < result[identifier].length; j++) {
+        for (let j = 0; j < result[identifier].length; j++) {
             const data = result[identifier][j]
             const sectionElement = document.createElement('div')
             sectionElement.classList.add('section-element')
@@ -223,15 +223,15 @@ function result(resultTab) {
             })
 
             // custom for each identifier
-            
+
             // Image
-            if(identifier === 'imageUrls') {
+            if (identifier === 'imageUrls') {
                 sectionElement.style.cssText = 'background-repeat: no-repeat;background-position: center center;background-size:cover;'
                 sectionElement.style.backgroundImage = `url(${getUrl(data)})`
                 sectionElement.setAttribute('data-copy', getUrl(data))
             }
             // Svg
-            else if(identifier === 'svgs') {
+            else if (identifier === 'svgs') {
                 sectionElement.removeAttribute('data-copy')
                 sectionElement.addEventListener('click', (e) => {
                     copyToClp(e.target.outerHTML)
@@ -270,7 +270,7 @@ function result(resultTab) {
                 count.innerHTML = `// count: ${data.counter}`
 
                 const pre = document.createElement('pre')
-                if(data.attributes.length === 0) {
+                if (data.attributes.length === 0) {
                     pre.innerHTML = `<b>.${data.className}</b> { }`
                 } else {
                     pre.innerHTML = `<b>.${data.className}</b> {\n    ${data.attributes.map(attribute => `${attribute.attr}: ${attribute.value};\n`).join('    ')}}`
@@ -283,7 +283,7 @@ function result(resultTab) {
                 sectionElement.addEventListener('click', (e) => {
                     copyToClp(e.target.querySelector('pre').innerHTML.replace('<b>', '').replace('</b>', ''))
                 })
-                
+
                 sectionElement.style.height = 'unset'
                 sectionElement.style.placeItems = 'unset'
                 sectionElement.style.padding = '8px'
@@ -304,12 +304,12 @@ function result(resultTab) {
 
     // add counters to sidebar
     const sidebarItems = document.querySelector('.sidebar').children
-    for(let i = 0; i < sidebarItems.length; i++) {
+    for (let i = 0; i < sidebarItems.length; i++) {
         const el = sidebarItems[i]
         const dataIdentifier = el.id.replace('sidebar-item-', '').replace(/-./g, (val) => {
             return val.charAt(1).toUpperCase()
         })
-        if(result[dataIdentifier]) el.querySelector('.sidebar-item--counter').innerHTML = result[dataIdentifier].length
+        if (result[dataIdentifier]) el.querySelector('.sidebar-item--counter').innerHTML = result[dataIdentifier].length
     }
 
     console.log(result)
@@ -320,7 +320,7 @@ function generateSidebar(data) {
     sidebarWrapper.innerHTML = ''
 
     const dataKeys = Object.keys(data)
-    for(let i = 0; i < dataKeys.length; i++) {
+    for (let i = 0; i < dataKeys.length; i++) {
         const identifier = dataKeys[i]
         const sidebarElement = document.createElement('div')
         sidebarElement.classList.add('sidebar-item')
@@ -332,7 +332,7 @@ function generateSidebar(data) {
         sidebarElement.addEventListener('click', (e) => {
             setActiveSectionByElement(e.target)
         })
-        
+
         sidebarWrapper.appendChild(sidebarElement)
     }
 
@@ -362,13 +362,13 @@ function generateSidebar(data) {
 
 function setActiveSectionByElement(el, id) {
     // change sidebar active element
-    if(activeSidebarItem) activeSidebarItem.classList.remove('sidebar-item--active')
+    if (activeSidebarItem) activeSidebarItem.classList.remove('sidebar-item--active')
     activeSidebarItem = el
     activeSidebarItem.classList.add('sidebar-item--active')
 
     // change display of section
-    if(activeSection) activeSection.style.top = '-9999999999px'
-    activeSection = document.getElementById(id) || document.getElementById(`section-${el.id.replace('sidebar-item-', '')}`) 
+    if (activeSection) activeSection.style.top = '-9999999999px'
+    activeSection = document.getElementById(id) || document.getElementById(`section-${el.id.replace('sidebar-item-', '')}`)
     activeSection.style.top = '0'
 }
 
@@ -376,13 +376,13 @@ function generateSections(data) {
     const sectionWrapper = document.body.querySelector('main')
     sectionWrapper.innerHTML = ''
     const dataKeys = Object.keys(data)
-    for(let i = 0; i < dataKeys.length; i++) {
+    for (let i = 0; i < dataKeys.length; i++) {
         const identifier = dataKeys[i]
         const sectionElement = document.createElement('section')
         sectionElement.id = `section-${identifier}`
 
         // adjust sections if needed
-        if(identifier === 'classes') {
+        if (identifier === 'classes') {
             sectionElement.style.gridTemplateColumns = '1fr'
         }
 
@@ -401,7 +401,7 @@ function generateSections(data) {
 }
 
 // thank you https://stackoverflow.com/questions/400212/how-do-i-copy-to-the-clipboard-in-javascript/51126086#51126086
-function copyToClp(txt){
+function copyToClp(txt) {
     var m = document
     txt = m.createTextNode(txt)
     var w = window
@@ -412,7 +412,7 @@ function copyToClp(txt){
         d.moveToElementText(txt)
         d.select()
         m.execCommand('copy')
-    } 
+    }
     else {
         var d = m.createRange()
         var g = w.getSelection
@@ -435,7 +435,7 @@ function copyToClp(txt){
 
 function getUrl(url) {
     const match = url.match(/(?<=url\(\")(.*)(?=\"\))/)
-    if(match) return match[0]
+    if (match) return match[0]
     return url
 }
 
